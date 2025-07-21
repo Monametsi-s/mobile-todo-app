51118b919fe022233529b672d72ff5a9e80444db
Monametsi <elboncaptain@gmail.com>
2025-07-22 00:17:03 +0200

    Add home layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,14 @@
+import { ThemeProvider } from "@/hooks/useTheme";
 import { Stack } from 'expo-router';
 // import IonIcons from '@expo/vector-icons/Ionicons';
 
 export default function RootLayout() {
   return (
-    <Stack screenOptions={{ headerShown: false }}>
+    <ThemeProvider>
+      <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="(tabs)" options={{ title: "Home"}}/>
     </Stack>
+    </ThemeProvider>
+    
   )
 }
