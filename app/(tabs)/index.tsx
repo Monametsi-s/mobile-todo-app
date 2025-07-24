25eaac05bd8622e91c0b2df47ddeb1ab78c92bbd
Monametsi <elboncaptain@gmail.com>
2025-07-24 17:08:31 +0200

    setting up styles

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,37 +1,23 @@
-import { Link } from 'expo-router';
-import { Text, View, StyleSheet } from 'react-native';
+import { createHomeStyles } from "@/assets/styles/home.styles";
+import useTheme, { ColorScheme } from "@/hooks/useTheme";
+import { Text, TouchableOpacity } from "react-native";
+import {SafeAreaView } from "react-native-safe-area-context"
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function Index() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.contentText} >
-        Welcome to the app!
-        </Text>
-        <Text> Hi </Text>
+  const { toggleDarkMode, colors } = useTheme();
 
-      <Link href="/settings">Vist about screen</Link>
-        </View>
+  const homeStyles = createHomeStyles(colors);
+
+  return (
+    <SafeAreaView style={homeStyles.safeArea}>
+      <LinearGradient colors={colors.gradients.background} style={homeStyles.container}>
+      <Text> Hi </Text>
+      <TouchableOpacity onPress={toggleDarkMode}>
+        <Text>Dark Mode</Text>
+      </TouchableOpacity>
+      </LinearGradient>
+    </SafeAreaView>
   );
 }
 
-
-
-
-
-
-
-
-
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    gap: 15
-  },
-  contentText: {
-    fontSize: 20,
-  }
-})
