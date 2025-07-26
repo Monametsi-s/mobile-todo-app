d7aa2528f8005258240b884b6c9c40440fc0891b
Monametsi <elboncaptain@gmail.com>
2025-07-26 21:59:19 +0200

    settings initial commit

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,7 +1,7 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-const About = () => {
+const Settings = () => {
   return (
     <View>
       <Text>This is expo about screen</Text>
@@ -9,4 +9,4 @@ const About = () => {
   )
 }
 
-export default About
+export default Settings
